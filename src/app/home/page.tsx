19c1ac9d712566470e1a57a3937a332357834339
Kallesh B S <kalleshbs47@gmail.com>
2025-03-29 14:22:30 +0530

    added basic api for testing

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
 
-function page() {
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+function Page() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const fetchMessage = async () => {
+            try {
+                const response = await fetch('https://nest-api-wg5t.onrender.com');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setMessage(data.message);
+            } catch (error:any) {
+                console.log('Error fetching message:', error);
+                setMessage(error.message)
+            }
+        };
+
+        fetchMessage();
+    }, []);
+
     return (
         <div className='h-screen flex justify-center items-center flex-col gap-2'>
             <h1>Deployed 👍</h1>
-            <Link passHref href={'/'} className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+            {message ? <p>{message}</p> : <span className="loading loading-spinner loading-xl"></span>} {/* Display the fetched message */}
+            <Link 
+                passHref 
+                href={'/'} 
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            >
                 Go Back
             </Link>
         </div>
     )
 }
 
-export default page
+export default Page;
